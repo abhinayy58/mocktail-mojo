2e0cb9f69b666f1c907ae82720a1ab77782c8cc0
Abhinay yadav <abhinayy48@gmail.com>
2026-01-20 15:20:39 +0530

    Add Cocktails component and integrate video in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+import { useRef } from "react";
 
 const Hero = () => {
+    const videoRef =  useRef(null);
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     useGSAP(() => {
         const  heroSplit = new SplitText(".title", { type: "chars, words" });
         const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -35,6 +39,26 @@ const Hero = () => {
         .to(".right-leaf", { y: 200}, 0)
         .to(".left-leaf",  { y: -200}, 0)
         
+
+      const startValue = isMobile ? "top 50%" : "center 60%";
+	const endValue = isMobile ? "120% top" : "bottom top";
+	
+	let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
+	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	};
+
 
         },[])
 
@@ -72,6 +96,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+        ref={videoRef}
+		 muted
+		 playsInline
+		 preload="auto"
+		 src="/videos/output.mp4"
+        ></video>
+
+      </div>
     </>
   );
 };
